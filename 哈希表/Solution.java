1ba40ea69fb6f840b8a8c7e74ff649a7d618e7ed
Eytins <eytins@foxmail.com>
2022-08-08 17:31:48 +0800

    增加哈希表练习代码

--- 哈希表/Solution.java
+++ 哈希表/Solution.java
@@ -229,6 +229,25 @@ public class Solution {
         return res;
     }
 
+    /**
+     * 1
+     *
+     * @param nums   n
+     * @param target t
+     * @return r
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{i, map.get(complement)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[2];
+    }
+
     public static void main(String[] args) {
         System.out.println(1 % 10);
     }
