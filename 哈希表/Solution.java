4a567d58697efe11227f9add76985e9c2d4c4461
Eytins <eytins@foxmail.com>
2022-08-09 21:59:13 +0800

    添加哈希表练习代码以及笔记

--- 哈希表/Solution.java
+++ 哈希表/Solution.java
@@ -248,6 +248,34 @@ public class Solution {
         return new int[2];
     }
 
+    /**
+     * 454
+     */
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i : nums1) {
+            for (int j : nums2) {
+                int key = i + j;
+                int value = map.getOrDefault(key, 0);
+                map.put(key, ++value);
+            }
+        }
+        for (int i : nums3) {
+            for (int j : nums4) {
+                int key = i + j;
+                if (map.containsKey(-key)) {
+                    count+=map.get(-key);
+                }
+            }
+        }
+        return count;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+    }
+
     public static void main(String[] args) {
         System.out.println(1 % 10);
     }
