3245a378feac9cb30c4c8321b03ebfc9c8e20446
Eytins <eytins@foxmail.com>
2022-08-15 17:54:54 +0800

    增加哈希表练习代码

--- 哈希表/Solution.java
+++ 哈希表/Solution.java
@@ -265,7 +265,7 @@ public class Solution {
             for (int j : nums4) {
                 int key = i + j;
                 if (map.containsKey(-key)) {
-                    count+=map.get(-key);
+                    count += map.get(-key);
                 }
             }
         }
@@ -273,7 +273,39 @@ public class Solution {
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
-        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] > 0) {
+                return result;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = length - 1;
+            while (right > left) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (right > left && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (right > left && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
