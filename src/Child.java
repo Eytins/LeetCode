b09704fa33697b259c32f8c18a4b58a45e82e678
Eytins <eytins@foxmail.com>
2023-08-26 15:17:47 +0100

    Some change

---
+++ src/Child.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Child {
+    private List<Grandchild> list = new ArrayList<>();
+
+    public List<Grandchild> getList() {
+        return list;
+    }
+
+    public void setList(List<Grandchild> list) {
+        this.list = list;
+    }
+
+
+}
---
+++ src/Grandchild.java
@@ -0,0 +1,15 @@
+    class Grandchild {
+        Grandchild(int a) {
+            this.a = a;
+        }
+        
+        private int a;
+
+        public int getA() {
+            return a;
+        }
+
+        public void setA(int a) {
+            this.a = a;
+        }
+    }
